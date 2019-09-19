8c547d83d1f8585546c4eb2b6a56486f253e8009
Bruno Brandão <48556994+Buorn@users.noreply.github.com>
2019-09-19 19:17:47 -0300

    Update l1ex10.c

--- mydir/l1ex10.c
+++ mydir/l1ex10.c
@@ -19,3 +19,37 @@ int main(void){
     printf("%d \n",n);
 return 0;
 }
+
+		   
+//código para passar vetor para matriz
+#include <stdio.h>
+
+int main()
+{
+    int i, j, n=0, col=0, lin=0;
+    char s[]="bruno bandeira brandao", mat[3][10];
+    
+    while(s[n] != '\0'){
+        n++;
+    }
+    
+    for(i=0; i<=n; i++){
+        if(s[i] == ' '){
+            col =0;
+            lin++;
+            i++;
+        }
+        mat[lin][col] = s[i];
+        col++;
+    }
+
+    for(i=0;i<=3;i++){
+        for(j=0;j<=10;j++){
+            printf("%c", mat[i][j]);
+        }
+    }
+
+
+    return 0;
+}
+		    
