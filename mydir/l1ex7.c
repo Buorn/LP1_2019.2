dcd8f9a9fa208c88d73066ba948a82fff797016d
Bruno Brandão <48556994+Buorn@users.noreply.github.com>
2019-09-29 23:51:47 -0300

    Update l1ex7.c

--- mydir/l1ex7.c
+++ mydir/l1ex7.c
@@ -1,61 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 
-//criação da estrutura
+
 	struct ficha{
-		// declaração das variaveis da estrutura
+		
 		char nome[10];
-		float nota1;
-		float nota2;
-		float media;
+		float nota;
 	};
 
-int main(void){
 
-	int i;
-	char aux[10];
-	float aux1, media=0;
-    //declarando uma variável para a estrutura criada
+
+
+int main(){
+	
+	int i, j;
+	float soma=0, media, aux;
+	char aux2[10];
+
 	struct ficha aluno[5];
-
+	
 	for(i=0; i<5; i++){
 		printf("Digite o nome do aluno: ");
 		scanf("%s", aluno[i].nome);
+	
+		printf("Digite a nota do aluno: ");
+		scanf("%f", &aluno[i].nota);
+	
 
-		printf("Digite a nota 1 do aluno: ");
-		scanf("%f", &aluno[i].nota1);
-
-		printf("Digite a nota 2 do aluno: ");
-		scanf("%f", &aluno[i].nota2);
 	}
-		for(i=0; i<5; i++){
-            aluno[i].media = (aluno[i].nota1+aluno[i].nota2)/2;
-		}
+	for(i=0; i<5; i++){
+	  printf("nome: %s\n", aluno[i].nome);
+	
+	  printf("nota1: %f\n", aluno[i].nota);
+		  
+	    soma+=aluno[i].nota;
+					
+	}
+	media=soma/5;
+	printf("Media: %2.2f\n", media);
+	for(i=0; i<5; i++){
+	        for(j=i+1; j<5; j++){
+	            if(aluno[j].nota>aluno[i].nota){
+	                aux = aluno[i].nota;
+	                strcpy(aux2, aluno[i].nome);
+			        aluno[i].nota = aluno[j].nota;
+	                strcpy(aluno[i].nome, aluno[j].nome);
+			        aluno[j].nota=aux;
+			        strcpy(aluno[j].nome,aux2);
+				        
+		        }
+	            
+	       }
+	}
 
-        for(i=0; i<5; i++){
-            for(j=i+1; j<5;j++){
-                if(aluno[i].media <= aluno[j].media){
-                    strcpy(aux, aluno[i].nome);
-                    strcpy(aluno[i].nome, aluno[j].nome);
-                    strcpy(aluno[j].nome, aux);
+	for(i=0; i<2; i++){  	  
+	    printf("%s: %2.2f \n",aluno[i].nome, aluno[i].nota);
 
-                    aux1 = aluno[i].media;
-                    aluno[i].media = aluno[j].media;
-                    aluno[j].media = aux1;
-                }
-            }
-        }		for(i=0; i<5; i++){
-            media+=aluno[i].media;
-		}
+	}
 
-		for(i=0; i<2; i++){
-            if(aluno[i].media > media){
-                printf("nome: %s\n", aluno[i].nome);
-                printf("nota1: %f\n", aluno[i].nota1);
-                printf("nota2: %f\n", aluno[i].nota2);
-                printf("media: %f\n", aluno[i].media);
-            }
-		}
 	return 0;
 
 }
