2184adf4504296fecbb040ce84bea7c9a72a8f47
Bruno Brandão <48556994+Buorn@users.noreply.github.com>
2019-09-30 01:25:18 -0300

    Update l1ex10.c

--- mydir/l1ex10.c
+++ mydir/l1ex10.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    int i, j=0, n=0, col=0, lin=0, npalavra=0, cont=0;
-    char s[]="bruno bandeira brandao", mat[3][10]={'\0'};
+    int i, j=0, k, n=0, col=0, lin=0, npalavra=0, cont;
+    char s[]="casa casa castelo", mat[3][10]={'\0'};
     //calcula tamanho da cadeia e o numero de palavras
     while(s[n] != '\0'){
         if(s[n] == ' '){
@@ -11,7 +11,7 @@ int main()
         }
         n++;
     }
-    
+
     //separa as palavras e joga para uma matriz
     for(i=0; i<=n; i++){
         if(s[i] == ' '){
@@ -23,13 +23,22 @@ int main()
         col++;
     }
     //imprime uma matriz de string
-    for(i=0;i<=3;i++){
-        printf("%s", mat[i]);
+    for(i=0;i<3;i++){
+        printf("%s \n", mat[i]);
     }
-    // conta numero de letras de cada string do vetor
-    for(i=0; i<=n; i++){
-        cont=0;
-      =0; i<=n; i++){
-
+    //compara as palavras de uma matriz
+    for(i=0;i<2;i++){
+        for(k=i+1;k<3;k++){
+            for(j=0;j<10;j++){
+                while(mat[i][j] == mat[k][j] && mat[i][j] != '\0' && mat[k][j] != '\0'){
+                    cont=0;
+                    if(mat[i][j] == '\0' && mat[k][j] == '\0'){
+                        cont++;
+                    }
+                }
+            }
+        }
+        printf("Nesta frase contem %d %s \n", cont, mat[i]);
+    }
     return 0;
 }
