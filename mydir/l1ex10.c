95cbc88350a09fcfa0482133ed949345ec86b33b
Bruno Brandão <48556994+Buorn@users.noreply.github.com>
2019-10-21 20:30:19 -0300

    Update l1ex10.c

--- mydir/l1ex10.c
+++ mydir/l1ex10.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int i, j=0, k, n=0, col=0, lin=0, npalavra=0, cont;
+    int i,nword, conf=0, j=0, n=0, col=0, lin=0, npalavra=0, cont=0;
     char s[]="casa casa castelo", mat[3][10]={'\0'};
     //calcula tamanho da cadeia e o numero de palavras
     while(s[n] != '\0'){
@@ -27,18 +27,25 @@ int main()
         printf("%s \n", mat[i]);
     }
     //compara as palavras de uma matriz
-    for(i=0;i<2;i++){
-        for(k=i+1;k<3;k++){
-            for(j=0;j<10;j++){
-                while(mat[i][j] == mat[k][j] && mat[i][j] != '\0' && mat[k][j] != '\0'){
-                    cont=0;
-                    if(mat[i][j] == '\0' && mat[k][j] == '\0'){
-                        cont++;
-                    }
-                }
+    lin=0;
+    for(i=lin+1;i<npalavra;i++){
+        nword=1;
+        for(j=0;j<10;j++){
+            if(mat[lin][j] !='\0'){
+                conf++;
+            }
+            if(mat[lin][j] == mat[i][j] && mat[lin][j] !='\0' && mat[i][j] != '\0'){
+                cont++;
             }
         }
-        printf("Nesta frase contem %d %s \n", cont, mat[i]);
+        if(conf==cont){
+            mat[i][0]='\0';
+            nword++;
+        }
+        lin++;
     }
+
+        printf("A mesma palavra aparece %i vezes nessa frase \n", nword);
+
     return 0;
 }
