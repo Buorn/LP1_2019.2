11bb448c394828d9fe0a5246607babb95cf0bea2
Bruno Brandão <48556994+Buorn@users.noreply.github.com>
2019-10-21 20:28:04 -0300

    Update l1e12.c

--- mydir/l1e12.c
+++ mydir/l1e12.c
@@ -3,7 +3,7 @@
 int main(void){
 
     int c, d, u, n;
-    char centena[13]= {'\0'} , dezena[10]= {'\0'} , unidade[7]={'\0'};
+    char centena[13]= {'\0'} , dezena[10]= {'\0'} , unidade[10]={'\0'};
 
     printf("Diite um valor:");
     scanf("%d", &n);
@@ -23,6 +23,7 @@ int main(void){
         d=0;
         u=n;
     }
+
     switch(c){
         case 0:
             strcpy(centena,"");
@@ -59,9 +60,6 @@ int main(void){
         case 0:
             strcpy(dezena,"");
             break;
-        case 1:
-            strcpy(dezena,"dez");
-            break;
         case 2:
             strcpy(dezena,"vinte");
             break;
@@ -87,39 +85,95 @@ int main(void){
             strcpy(dezena,"noventa");
             break;
     }
-    switch(u){
-        case 0:
-            strcpy(unidade,"");
-            break;
-        case 1:
-            strcpy(unidade,"um");
-            break;
-        case 2:
-            strcpy(unidade,"dois");
-            break;
-        case 3:
-            strcpy(unidade,"tres");
-            break;
-        case 4:
-            strcpy(unidade,"quatro");
-            break;
-        case 5:
-            strcpy(unidade,"cinco");
-            break;
-        case 6:
-            strcpy(unidade,"seis");
-            break;
-        case 7:
-            strcpy(unidade,"sete");
-            break;
-        case 8:
-            strcpy(unidade,"oito");
-            break;
-        case 9:
-            strcpy(unidade,"nove");
-            break;
-    }
-    printf("%s e %s e %s \n", centena, dezena, unidade);
 
+    if(d==1){
+        switch(u){
+            case 0:
+                strcpy(unidade,"dez");
+                break;
+            case 1:
+                strcpy(unidade,"onze");
+                break;
+            case 2:
+                strcpy(unidade,"doze");
+                break;
+            case 3:
+                strcpy(unidade,"treze");
+                break;
+            case 4:
+                strcpy(unidade,"catorze");
+                break;
+            case 5:
+                strcpy(unidade,"quinze");
+                break;
+            case 6:
+                strcpy(unidade,"dezeseis");
+                break;
+            case 7:
+                strcpy(unidade,"dezesete");
+                break;
+            case 8:
+                strcpy(unidade,"dezoito");
+                break;
+            case 9:
+                strcpy(unidade,"dezenove");
+                break;
+        }
+    }
+    else{
+        switch(u){
+            case 0:
+                strcpy(unidade,"");
+                break;
+            case 1:
+                strcpy(unidade,"um");
+                break;
+            case 2:
+                strcpy(unidade,"dois");
+                break;
+            case 3:
+                strcpy(unidade,"tres");
+                break;
+            case 4:
+                strcpy(unidade,"quatro");
+                break;
+            case 5:
+                strcpy(unidade,"cinco");
+                break;
+            case 6:
+                strcpy(unidade,"seis");
+                break;
+            case 7:
+                strcpy(unidade,"sete");
+                break;
+            case 8:
+                strcpy(unidade,"oito");
+                break;
+            case 9:
+                strcpy(unidade,"nove");
+                break;
+        }
+    }
+    if(n>100 && d!=1 && d!=0 && u!=0){
+        printf("%s e %s e %s \n", centena, dezena, unidade);
+    }
+    if(n>100 && d==0){
+        printf("%s e %s \n", centena, unidade);
+    }
+    if(n>100 && d==1){
+        printf("%s e %s \n", centena, unidade);
+    }
+    if(n>100 && u==0){
+        printf("%s e %s \n", centena, dezena);
+    }
+    if(n<100 && n >19){
+        printf("%s e %s \n", dezena, unidade);
+    }
+    if(n<20 && n>10 && d==1){
+        printf("%s \n", unidade);
+    }
+    if(n<=10){
+        printf("%s \n", unidade);
+    }
     return 0;
     }
